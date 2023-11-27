32cca035ad657992d55ade61336fa7540d64b8c7
SuperFASS <fabrisorrentino@hotmail.com>
2023-11-27 15:57:35 -0300

    added functions: onDelete onEdit onCreate modified gitignore

--- frontend/lib/db.api.tsx
+++ frontend/lib/db.api.tsx
@@ -1,16 +1,54 @@
 export const onDelete = (selected: any) => {
   console.log("onDelete", selected);
+  return fetch("http://127.0.0.1:3001/books/{selected}",  {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(selected),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
 };
 
 export const onEdit = (data: any) => {
   console.log("onEdit", data);
+  return fetch("http://127.0.0.1:3001/books/{data.id}", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
 };
 
 export const onCreate = (data: any) => {
   console.log("onCreate", data);
+  return fetch("http://127.0.0.1:3001/books", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
+  
 };
 
 export const onSearch = () => {
+  console.log("onSearch");
   return fetch("http://127.0.0.1:3001/books")
     .then((res) => res.json())
     .then((data) => {
@@ -23,9 +61,12 @@ export const onGenerarPdf = () => {
   console.log("onGenerarPdf");
 };
 
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   onDelete,
   onEdit,
+  onSearch,
   onCreate,
+  onGenerarPdf,
 };
